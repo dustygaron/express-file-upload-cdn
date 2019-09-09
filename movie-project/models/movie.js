a0572671afc4c5cfbfe6813074eca1db03b20e33
Dusty <dustygaron@hotmail.com>
2019-09-09 11:42:56 -0400

    Done

---
+++ movie-project/models/movie.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const movieSchema = new Schema({
+  title: String,
+  description: String,
+  imgName: String,
+  imgPath: String
+}, {
+    timestamps: {
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt'
+    }
+  })
+
+let Movie = mongoose.model('Movie', movieSchema)
+module.exports = Movie
---
+++ movie-project/routes/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const Movie = require('../models/movie.js')
+const uploadCloud = require('../config/cloudinary.js')
+const router = express.Router()
+
+/* GET home page */
+router.get('/', (req, res, next) => {
+  Movie.find()
+    .then((movies) => {
+      res.render('index', { movies })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
+
+router.get('/movie/add', (req, res, next) => {
+  res.render('movie-add')
+})
+
+router.post('/movie/add', uploadCloud.single('photo'), (req, res, next) => {
+  const { title, description } = req.body
+  const imgPath = req.file.url
+  const imgName = req.file.originalname
+  const newMovie = new Movie({ title, description, imgPath, imgName })
+  newMovie.save()
+    .then(movie => {
+      res.redirect('/')
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+
+module.exports = router
